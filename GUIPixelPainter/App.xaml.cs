7610ac21557442dfcd64f5b4ee1cb654459d95e3
Егор Мозговой <egor08281@gmail.com>
2019-11-30 13:52:23 +0300

    start license impl

--- GUIPixelPainter/App.xaml.cs
+++ GUIPixelPainter/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace GUIPixelPainter
 {
@@ -7,5 +8,28 @@ namespace GUIPixelPainter
     /// </summary>
     public partial class App : Application
     {
+        private bool checkLicense = false;
+        private RoutedEventHandler onStartup;
+
+        public App(bool checkLicense, RoutedEventHandler onStartup) : base()
+        {
+            this.checkLicense = checkLicense;
+            this.onStartup = onStartup;
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (checkLicense)
+            {
+                GUI.LicenseWindow licenseCheck = new GUI.LicenseWindow();
+                licenseCheck.ShowDialog();
+                if (!licenseCheck.success)
+                {
+                    this.Shutdown();
+                    return;
+                }
+            }
+            GUI.BotWindow botWindow = new GUI.BotWindow();
+            botWindow.Loaded += onStartup;
+        }
     }
 }
